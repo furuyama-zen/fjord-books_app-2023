201802abc137586656a1a19c588f19f4a9327006
furuyama-zen <naoki.furuyama@zeneffect.co.jp>
2024-09-10 17:41:19 +0900

    Userテーブルと画面にカラム追加して日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postalcode, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postalcode, :address, :profile])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:postalcode, :address, :profile])
+  end
 end
