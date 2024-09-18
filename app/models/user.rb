ddc0801ba0d2f10fcc7a6606423d76ca5f17972a
furuyama-zen <naoki.furuyama@zeneffect.co.jp>
2024-09-18 15:29:06 +0900

    activestorageの実装とi18nで日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  validate :image_content_type
+
+  def image_content_type
+    return unless image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    errors.add(I18n.t('activestorage.image'), '：ファイル形式が、JPEG, PNG, GIF以外になっています。ファイル形式をご確認ください。')
+  end
 end
