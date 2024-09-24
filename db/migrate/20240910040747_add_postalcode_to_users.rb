8a942b0fcbc95be93480215c2351d1821fb19bb8
furuyama-zen <naoki.furuyama@zeneffect.co.jp>
2024-09-24 12:39:07 +0900

    不要なmigrationファイル削除、フォームのhuman_attribute_nameを削除

--- db/migrate/20240910040747_add_postalcode_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPostalcodeToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :postalcode, :string
-  end
-end
--- db/migrate/20240919063812_remove_columns_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :postalcode, :string
-    remove_column :users, :address, :string
-    remove_column :users, :profile, :text
-  end
-end
